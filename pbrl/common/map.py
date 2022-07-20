f16a487e8c34287703561bb69f0b4d33948449d9
jjccero <1127986703@qq.com>
2022-07-20 12:00:35 +0800

    fix automap and merge map, add rms.extend

--- pbrl/common/map.py
+++ pbrl/common/map.py
@@ -4,9 +4,9 @@ import torch
 
 def auto_map(f, x, **kwargs):
     if isinstance(x, tuple):
-        return tuple(auto_map(f, e) for e in x)
+        return tuple(auto_map(f, e, **kwargs) for e in x)
     elif isinstance(x, dict):
-        return {k: auto_map(f, v) for k, v in x.items()}
+        return {k: auto_map(f, v, **kwargs) for k, v in x.items()}
     else:
         return f(x, **kwargs)
 
@@ -15,9 +15,9 @@ def merge_map(f, x, **kwargs):
     # x must be iterable
     first_item = x[0]
     if isinstance(first_item, tuple):
-        return tuple(merge_map(f, e) for e in zip(*x))
+        return tuple(merge_map(f, e, **kwargs) for e in zip(*x))
     elif isinstance(first_item, dict):
-        return {k: merge_map(f, tuple(e[k] for e in x)) for k in first_item}
+        return {k: merge_map(f, tuple(e[k] for e in x), **kwargs) for k in first_item}
     else:
         return f(x, **kwargs)
 
--- pbrl/common/rms.py
+++ pbrl/common/rms.py
@@ -4,41 +4,56 @@ from pbrl.common.map import map_space, merge_map
 
 
 def normalize(data, rms):
-    x, mean, var = data
+    x, mean, std = data
     if rms.reduce_mean:
         x = x - mean
-    y = x / np.sqrt(var + rms.eps)
+    y = x / (std + rms.eps)
     if rms.clip is not None:
         y = np.clip(y, -rms.clip, rms.clip)
     return y
 
 
 def update(data, rms):
-    x, mean, var = data
+    x, mean, std = data
     n = x.shape[0]
     n_new = rms.n + n
     local_mean = x.mean(axis=0)
     delta = local_mean - mean
     mean_new = mean + delta * n / n_new
     local_var = x.var(axis=0)
-    m_a = var * rms.n
+    m_a = std ** 2 * rms.n
     m_b = local_var * n
     m_2 = m_a + m_b + delta ** 2 * rms.n * n / n_new
     var_new = m_2 / n_new
 
     mean[:] = mean_new
-    var[:] = var_new
+    std[:] = np.sqrt(var_new)
     rms.n_new = n_new
+
+
+def extend(data, rms1, rms2):
+    mean1, std1, mean2, std2 = data
+    n_new = rms1.n + rms2.n
+    delta = mean2 - mean1
+    mean_new = mean1 + delta * rms2.n / n_new
+    m_a = std1 ** 2 * rms1.n
+    m_b = std2 ** 2 * rms2.n
+    m_2 = m_a + m_b + delta ** 2 * rms1.n * rms2.n / n_new
+    var_new = m_2 / n_new
+
+    mean1[:] = mean_new
+    std1[:] = np.sqrt(var_new)
+    rms1.n_new = n_new
 
 
 class RunningMeanStd:
     def __init__(self, space, clip, reduce_mean):
         if space is None:
             self.mean = np.zeros(1, np.float64)
-            self.var = np.ones(1, np.float64)
+            self.std = np.ones(1, np.float64)
         else:
             self.mean = map_space(lambda x: np.zeros(x.shape, np.float64), space)
-            self.var = map_space(lambda x: np.ones(x.shape, np.float64), space)
+            self.std = map_space(lambda x: np.ones(x.shape, np.float64), space)
         self.n = 0
         self.n_new = 0
         self.eps = 1e-8
@@ -49,15 +64,19 @@ class RunningMeanStd:
         if o is None:
             return
         self.mean = o.mean
-        self.var = o.var
+        self.std = o.std
         self.n = o.n
         self.eps = o.eps
         self.clip = o.clip
         self.reduce_mean = o.reduce_mean
 
     def update(self, x):
-        merge_map(update, (x, self.mean, self.var), rms=self)
+        merge_map(update, (x, self.mean, self.std), rms=self)
+        self.n = self.n_new
+
+    def extend(self, o):
+        merge_map(extend, (self.mean, self.std, o.mean, o.std), rms1=self, rms2=o)
         self.n = self.n_new
 
     def normalize(self, x):
-        return merge_map(normalize, (x, self.mean, self.var), rms=self)
+        return merge_map(normalize, (x, self.mean, self.std), rms=self)
